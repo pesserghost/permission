762c6f5908666b0af1889beecce91f94d045a07c
Shohruxbek <email@manzil.com>
2025-04-10 23:10:40 +0500

    permission

--- app/models.py
+++ app/models.py
@@ -1,20 +1,43 @@
-from rest_framework import permissions
-from .models import Teacher
+from django.db import models
+from django.core.validators import (MinValueValidator, MaxValueValidator,
+                                    FileExtensionValidator)
+from django.contrib.auth.models import User
 
-class IsAuthenticatedOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:  # GET requests
-            return True
-        return request.user.is_authenticated
+# Create your models here.
 
-class IsTeacher(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:  # Allow read for all
-            return True
-        return request.user.is_authenticated and Teacher.objects.filter(user=request.user).exists()
 
-class IsCourseCreator(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user.is_authenticated and obj.teacher.user == request.user
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="teacher/teachers/", null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150)
+    duration = models.IntegerField(default=8, validators=[
+        MinValueValidator(1),
+        MaxValueValidator(12)
+    ])
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    theme = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    text = models.TextField(null=True, blank=True)
+    photo = models.ImageField(upload_to="lesson/photos/", null=True, blank=True)
+    video = models.FileField(upload_to="lesson/videos/", validators=[
+        FileExtensionValidator(allowed_extensions=['mp4'])
+    ], null=True, blank=True)
+    views = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.theme
---
+++ app/permission.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions
+from .models import Teacher
+
+class IsAuthenticatedOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:  # GET requests
+            return True
+        return request.user.is_authenticated
+
+class IsTeacher(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:  # Allow read for all
+            return True
+        return request.user.is_authenticated and Teacher.objects.filter(user=request.user).exists()
+
+class IsCourseCreator(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and obj.teacher.user == request.user
